919b18ffc373dd8f4f5dbeed58b9b618709c3fd5
Muhammad Khalid <muhammadakhalid04@gmail.com>
2019-04-24 23:47:19 -0400

    final change

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+/**
+ * @author Team 4
+ * @version
+ * @since
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+//SalesLead - add information (name, phone number, email, salespeople id)
+        SalesLead s1 = new SalesLead();
+        s1.setSalesLeadID(0);
+
+        SalesLead s2 = new SalesLead();
+        s2.setSalesLeadID(1);
+
+        SalesLead s3 = new SalesLead();
+        s3.setSalesLeadID(2);
+
+        SalesLead s4 = new SalesLead();
+        s4.setSalesLeadID(3);
+
+        SalesLead s5 = new SalesLead();
+        s5.setSalesLeadID(4);
+    }
+}
